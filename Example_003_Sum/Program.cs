347cd6e6e6f7166cde886ab4e9547c437060f039
SoX <arksurviving5@gmail.com>
2022-09-28 22:53:32 +0300

    Example 003 Sum Random two numbers

--- Example_003_Sum/Program.cs
+++ Example_003_Sum/Program.cs
@@ -1,3 +1,7 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(1, 10);
+int numberB = new Random().Next(1, 10);
+Console.Write(numberA);
+Console.Write(" * ");
+Console.Write(numberB);
+Console.Write(" = ");
 Console.WriteLine (numberA + numberB);
