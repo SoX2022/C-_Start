efd39a5b4ca3c13235149cba8435555a01290a7a
SoX <arksurviving5@gmail.com>
2022-10-01 16:18:26 +0300

    added Example 008 Two Friends And A Dog

---
+++ Example_008_TwoFriendsAndADog/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.Write("Enter first's friend speed: ");
+int firstFriendSpeed = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Enter second's friend speed: ");
+int secondFriendSpeed = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Enter dog's speed: ");
+int dogSpeed = int.Parse(Console.ReadLine() ?? "");
+Console.Write("What distance between two friends is? ");
+int distance = int.Parse(Console.ReadLine() ?? "");
+int count = 0;
+int dogDirection = 2;
+
+while (distance > dogSpeed + firstFriendSpeed + secondFriendSpeed)
+{
+    if (dogDirection == 2)
+    {
+        count++;
+        int time = distance / (dogSpeed + secondFriendSpeed);
+        distance = distance - time * (firstFriendSpeed + secondFriendSpeed);
+        dogDirection = 1;
+    }
+    else
+    {
+        count++;
+        int time = distance / (dogSpeed + firstFriendSpeed);
+        distance = distance - time * (firstFriendSpeed + secondFriendSpeed);
+        dogDirection = 2;
+    }
+}
+
+Console.Clear();
+Console.Write("The dog can check friends " + count);
+
+if (count == 1)
+{
+    Console.Write(" time ");
+}
+else
+{
+    Console.Write(" times ");
+}
+
+Console.WriteLine("before they will meet each other.");
