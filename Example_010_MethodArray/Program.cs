bc0f9480c849407549b6e897af663ce9575a7a24
SoX <arksurviving5@gmail.com>
2022-10-02 13:17:33 +0300

    added Example 010 Method Array

---
+++ Example_010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 1, 4, 3, 4, 5, 6, 7, 8, 9 };
+
+int lenght = array.Length;
+int find = 10;
+int index = 0;
+
+while (index < lenght)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
+
+
