a3523a8a031c92bdd987acdcffb5e8f2d7dde659
SoX <arksurviving5@gmail.com>
2022-10-02 12:59:36 +0300

    added Example 008 Intro Method

---
+++ Example_008_IntroMethod/Program.cs
@@ -0,0 +1,31 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 14;
+int b1 = 23;
+int c1 = 23;
+
+int a2 = 222;
+int b2 = 2;
+int c2 = 3;
+
+int a3 = 42;
+int b3 = 21;
+int c3 = 71;
+
+//int max1 = Max(a1, b1, c1);
+//int max2 = Max(a2, b2, c2);
+//int max3 = Max(a3, b3, c3);
+//int max = Max(max1, max2, max3);
+
+int max = Max(
+    Max(a1, b1, c1),
+    Max(a2, b2, c2),
+    Max(a3, b3, c3));
+
+Console.WriteLine(max);
